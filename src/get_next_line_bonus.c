65c0326aaa0498f41b330918b80e6e3fa8a11961
Vojan-Najov <serejah.mashichev@gmail.com>
2023-08-17 19:35:02 +0300

    To clang-format.

--- src/get_next_line_bonus.c
+++ src/get_next_line_bonus.c
@@ -12,116 +12,95 @@
 
 #include "get_next_line_bonus.h"
 
-static char		*check_buffer(t_file *file, char **line);
+static char *check_buffer(t_file *file, char **line);
 
-static int		read_file_until_nl(t_file *file, char **line);
+static int read_file_until_nl(t_file *file, char **line);
 
-static t_file	*new_file(int fd);
+static t_file *new_file(int fd);
 
-static t_file	*get_file(t_file **head, int fd);
+static t_file *get_file(t_file **head, int fd);
 
-int	get_next_line(int fd, char **line)
-{
-	static t_file	*head;
-	t_file			*file;
-	char			*ptr_nl;
-	int				n_read;
+int get_next_line(int fd, char **line) {
+  static t_file *head;
+  t_file *file;
+  char *ptr_nl;
+  int n_read;
 
-	file = get_file(&head, fd);
-	if (!file)
-		return (-1);
-	ptr_nl = check_buffer(file, line);
-	if (!ptr_nl && *line)
-		n_read = read_file_until_nl(file, line);
-	if (!*line || n_read == -1)
-	{
-		if (*line)
-			free(*line);
-		*line = NULL;
-		return (remove_file(&head, fd));
-	}
-	if (n_read > 0 || *file->buf || ptr_nl)
-		return (1);
-	remove_file(&head, fd);
-	return (0);
+  file = get_file(&head, fd);
+  if (!file) return (-1);
+  ptr_nl = check_buffer(file, line);
+  if (!ptr_nl && *line) n_read = read_file_until_nl(file, line);
+  if (!*line || n_read == -1) {
+    if (*line) free(*line);
+    *line = NULL;
+    return (remove_file(&head, fd));
+  }
+  if (n_read > 0 || *file->buf || ptr_nl) return (1);
+  remove_file(&head, fd);
+  return (0);
 }
 
-static char	*check_buffer(t_file *file, char **line)
-{
-	char	*ptr_nl;
+static char *check_buffer(t_file *file, char **line) {
+  char *ptr_nl;
 
-	ptr_nl = ft_strchr(file->buf, '\n');
-	if (ptr_nl)
-	{
-		*ptr_nl = '\0';
-		*line = ft_strdup(file->buf);
-		++ptr_nl;
-		ft_strcpy(file->buf, ptr_nl);
-	}
-	else
-	{
-		*line = ft_strdup(file->buf);
-		*file->buf = '\0';
-	}
-	return (ptr_nl);
+  ptr_nl = ft_strchr(file->buf, '\n');
+  if (ptr_nl) {
+    *ptr_nl = '\0';
+    *line = ft_strdup(file->buf);
+    ++ptr_nl;
+    ft_strcpy(file->buf, ptr_nl);
+  } else {
+    *line = ft_strdup(file->buf);
+    *file->buf = '\0';
+  }
+  return (ptr_nl);
 }
 
-static int	read_file_until_nl(t_file *file, char **line)
-{
-	int		n_read;
-	char	*ptr_nl;
+static int read_file_until_nl(t_file *file, char **line) {
+  int n_read;
+  char *ptr_nl;
 
-	n_read = read(file->fd, file->buf, BUFFER_SIZE);
-	while (n_read > 0)
-	{
-		*(file->buf + n_read) = '\0';
-		ptr_nl = ft_strchr(file->buf, '\n');
-		if (ptr_nl)
-		{
-			*ptr_nl = '\0';
-			*line = ft_strjoin_and_free_s1(*line, file->buf);
-			++ptr_nl;
-			ft_strcpy(file->buf, ptr_nl);
-			break ;
-		}
-		else
-		{	
-			*line = ft_strjoin_and_free_s1(*line, file->buf);
-			*file->buf = '\0';
-		}
-		n_read = read(file->fd, file->buf, BUFFER_SIZE);
-	}
-	return (n_read);
+  n_read = read(file->fd, file->buf, BUFFER_SIZE);
+  while (n_read > 0) {
+    *(file->buf + n_read) = '\0';
+    ptr_nl = ft_strchr(file->buf, '\n');
+    if (ptr_nl) {
+      *ptr_nl = '\0';
+      *line = ft_strjoin_and_free_s1(*line, file->buf);
+      ++ptr_nl;
+      ft_strcpy(file->buf, ptr_nl);
+      break;
+    } else {
+      *line = ft_strjoin_and_free_s1(*line, file->buf);
+      *file->buf = '\0';
+    }
+    n_read = read(file->fd, file->buf, BUFFER_SIZE);
+  }
+  return (n_read);
 }
 
-static t_file	*new_file(int fd)
-{
-	t_file	*file;
+static t_file *new_file(int fd) {
+  t_file *file;
 
-	file = (t_file *) malloc(sizeof(*file));
-	if (file)
-	{
-		file->fd = fd;
-		file->buf = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!file->buf)
-		{
-			free(file);
-			return (NULL);
-		}
-		*file->buf = '\0';
-		file->next = NULL;
-	}
-	return (file);
+  file = (t_file *)malloc(sizeof(*file));
+  if (file) {
+    file->fd = fd;
+    file->buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!file->buf) {
+      free(file);
+      return (NULL);
+    }
+    *file->buf = '\0';
+    file->next = NULL;
+  }
+  return (file);
 }
 
-static t_file	*get_file(t_file **head, int fd)
-{
-	while (*head)
-	{
-		if ((*head)-> fd == fd)
-			return (*head);
-		head = &(*head)->next;
-	}
-	*head = new_file(fd);
-	return (*head);
+static t_file *get_file(t_file **head, int fd) {
+  while (*head) {
+    if ((*head)->fd == fd) return (*head);
+    head = &(*head)->next;
+  }
+  *head = new_file(fd);
+  return (*head);
 }
--- src/get_next_line_utils_bonus.c
+++ src/get_next_line_utils_bonus.c
@@ -12,86 +12,71 @@
 
 #include "get_next_line_bonus.h"
 
-char	*ft_strcpy(char *dst, char const *src)
-{
-	char	*d;
+char *ft_strcpy(char *dst, char const *src) {
+  char *d;
 
-	d = dst;
-	while (*src)
-		*d++ = *src++;
-	*d = '\0';
-	return (dst);
+  d = dst;
+  while (*src) *d++ = *src++;
+  *d = '\0';
+  return (dst);
 }
 
-char	*ft_strchr(const char *s, int c)
-{
-	char	smb;
+char *ft_strchr(const char *s, int c) {
+  char smb;
 
-	smb = (char) c;
-	while (*s)
-		if (*s++ == smb)
-			return ((char *)--s);
-	if (smb == '\0')
-		return ((char *) s);
-	else
-		return (NULL);
+  smb = (char)c;
+  while (*s)
+    if (*s++ == smb) return ((char *)--s);
+  if (smb == '\0')
+    return ((char *)s);
+  else
+    return (NULL);
 }
 
-char	*ft_strdup(const char *s1)
-{
-	char		*dup;
-	const char	*str;
+char *ft_strdup(const char *s1) {
+  char *dup;
+  const char *str;
 
-	str = s1;
-	while (*str)
-		++str;
-	dup = (char *) malloc(sizeof(*dup) * (str - s1 + 1));
-	if (dup)
-		ft_strcpy(dup, s1);
-	return (dup);
+  str = s1;
+  while (*str) ++str;
+  dup = (char *)malloc(sizeof(*dup) * (str - s1 + 1));
+  if (dup) ft_strcpy(dup, s1);
+  return (dup);
 }
 
-char	*ft_strjoin_and_free_s1(char *s1, const char *s2)
-{
-	char		*str;
-	char		*str1;
-	const char	*str2;
-	size_t		s1_len;
-	size_t		s2_len;
+char *ft_strjoin_and_free_s1(char *s1, const char *s2) {
+  char *str;
+  char *str1;
+  const char *str2;
+  size_t s1_len;
+  size_t s2_len;
 
-	str1 = s1;
-	while (*str1)
-		++str1;
-	str2 = s2;
-	while (*str2)
-		++str2;
-	s1_len = str1 - s1;
-	s2_len = str2 - s2;
-	str = (char *) malloc(sizeof(*str) * (s1_len + s2_len + 1));
-	if (str)
-	{
-		ft_strcpy(str, s1);
-		ft_strcpy(str + s1_len, s2);
-	}
-	free(s1);
-	return (str);
+  str1 = s1;
+  while (*str1) ++str1;
+  str2 = s2;
+  while (*str2) ++str2;
+  s1_len = str1 - s1;
+  s2_len = str2 - s2;
+  str = (char *)malloc(sizeof(*str) * (s1_len + s2_len + 1));
+  if (str) {
+    ft_strcpy(str, s1);
+    ft_strcpy(str + s1_len, s2);
+  }
+  free(s1);
+  return (str);
 }
 
-int	remove_file(t_file **head, int fd)
-{
-	t_file	*tmp;
+int remove_file(t_file **head, int fd) {
+  t_file *tmp;
 
-	while (*head)
-	{
-		if ((*head)->fd == fd)
-		{
-			tmp = *head;
-			*head = (*head)->next;
-			free(tmp->buf);
-			free(tmp);
-		}
-		else
-			head = &(*head)->next;
-	}
-	return (-1);
+  while (*head) {
+    if ((*head)->fd == fd) {
+      tmp = *head;
+      *head = (*head)->next;
+      free(tmp->buf);
+      free(tmp);
+    } else
+      head = &(*head)->next;
+  }
+  return (-1);
 }
